868f187168b87fcd5371ad0cecd6c6180bd08a4e
luis nunez <l.nunez@blackstone.studio>
2026-03-23 14:04:57 -0600

    terminar metodo de guardar

--- controllers/project.js
+++ controllers/project.js
@@ -1,9 +1,34 @@
 const Project = require('../models/project');
 
 const saveProject = async (req, res) => {
-    return res.status(200).send({
-        message: 'Probando controlador'
-    });
+
+    const body = req.body;
+
+    if (!body.name || !body.description || !body.state) {
+        return res.status(400).send({
+            status: 'error',
+            message: 'Faltan datos por enviar'
+        });
+    }
+
+    let projectToSave = new Project(body);
+
+    try {
+        const projectStored = await projectToSave.save();
+
+        return res.status(200).send({
+            status: 'success',
+            project: projectStored
+        });
+    } catch (error) {
+        return res.status(500).send({
+            status: 'error',
+            message: 'Error al guardar el proyecto',
+            error
+        });
+    }
+
+
 }
 
 module.exports = {
