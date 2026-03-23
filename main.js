6572158494ec96536268338198095874d48ac061
luis nunez <l.nunez@blackstone.studio>
2026-03-23 09:00:30 -0600

    mcp tipo de cambio

--- main.js
+++ main.js
@@ -1,121 +1,78 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 // crear servidor
 const servidor = new McpServer({
-    name: "servidor-mcp-calculadora",
+    name: "servidor-mcp-monedas",
     version: "1.0.0"
 });
-// crear las herramientas
-servidor.registerTool('sumar', {
-    title: 'Suma dos numeros',
-    description: 'Recibe dos numeros y los suma',
+// crear una herramienta para sacar el valor de una moneda
+servidor.registerTool('valor_monedas', {
+    title: 'Conseguir el valor de una moneda',
+    description: 'Recibe el nombre de una moneda y devuelve su valor',
     inputSchema: {
-        numero1: z.number(),
-        numero2: z.number()
+        moneda: z.string().min(1, "Debe ingresar una moneda, por ejemplo: EUR, USD")
     }
-}, async ({ numero1, numero2 }) => {
-    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
-        throw new Error('Los valores deben ser números');
+}, async ({ moneda }) => {
+    //https://cdn.moneyconvert.net/api/latest.json
+    if (typeof moneda !== 'string') {
+        throw new Error('La moneda debe ser una cadena de texto');
     }
+    const url = `https://cdn.moneyconvert.net/api/latest.json`;
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+        throw new Error(`Error al acceder a la api`);
+    }
+    const datos = await respuesta.json();
+    const base = 'USD';
+    if (!datos.rates[moneda]) {
+        throw new Error(`No se encontro el valor de la moneda ${moneda}`);
+    }
+    const value = datos.rates[moneda];
     return {
         content: [
             {
                 type: 'text',
-                text: String(numero1 + numero2)
+                text: `El valor actual de la moneda ${moneda.toUpperCase()} es ${value} frente a ${base}`
             }
         ]
     };
 });
-servidor.registerTool('restar', {
-    title: 'Resta dos numeros',
-    description: 'Recibe dos numeros y los resta',
+//crear herramienta para convertir una cifra al valor de otra moneda
+servidor.registerTool('convertir_tipo_cambio', {
+    title: 'Convertir una cifra al valor de otra moneda',
+    description: 'Devuleve el valor de una moneda frente a otra',
     inputSchema: {
-        numero1: z.number(),
-        numero2: z.number()
+        moneda_origen: z.string().min(3, "Debe ingresar una moneda, por ejemplo: EUR, USD"),
+        moneda_destino: z.string().min(3, "Debe ingresar una moneda, por ejemplo: EUR, USD"),
+        cantidad: z.number()
     }
-}, async ({ numero1, numero2 }) => {
-    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
-        throw new Error('Los valores deben ser números');
+}, async ({ moneda_origen, moneda_destino, cantidad }) => {
+    //https://cdn.moneyconvert.net/api/latest.json
+    const url = `https://cdn.moneyconvert.net/api/latest.json`;
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+        throw new Error(`Error al acceder a la api`);
     }
+    const datos = await respuesta.json();
+    const { rates } = datos;
+    const base = 'USD';
+    if (!rates || !rates) {
+        throw new Error(`No se encontro el valor de la moneda`);
+    }
+    let rate;
+    if (moneda_origen === base) {
+        rate = rates[moneda_destino];
+    }
+    else {
+        rate = rates[moneda_destino] / rates[moneda_origen];
+    }
+    const value_converted = cantidad * rate;
     return {
         content: [
             {
                 type: 'text',
-                text: String(numero1 - numero2)
-            }
-        ]
-    };
-});
-servidor.registerTool('multiplicar', {
-    title: 'Multiplicar numeros',
-    description: 'Multiplica dos números',
-    inputSchema: {
-        numero1: z.number(),
-        numero2: z.number()
-    }
-}, async ({ numero1, numero2 }) => {
-    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
-        throw new Error('Los valores deben ser números');
-    }
-    return {
-        content: [
-            {
-                type: 'text',
-                text: String(numero1 * numero2)
-            }
-        ]
-    };
-});
-servidor.registerTool('dividir', {
-    title: 'Dividir numeros',
-    description: 'Divide dos números',
-    inputSchema: {
-        numero1: z.number(),
-        numero2: z.number()
-    }
-}, async ({ numero1, numero2 }) => {
-    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
-        throw new Error('Los valores deben ser números');
-    }
-    return {
-        content: [
-            {
-                type: 'text',
-                text: String(numero1 / numero2)
-            }
-        ]
-    };
-});
-servidor.registerTool('resto_division', {
-    title: 'Resto de la división',
-    description: 'Recibe dos números y calcula el resto de la división',
-    inputSchema: {
-        numero1: z.number(),
-        numero2: z.number()
-    }
-}, async ({ numero1, numero2 }) => {
-    if (typeof numero1 !== 'number' || typeof numero2 !== 'number') {
-        throw new Error('Los valores deben ser números');
-    }
-    return {
-        content: [
-            {
-                type: 'text',
-                text: String(numero1 % numero2)
-            }
-        ]
-    };
-});
-servidor.registerResource('saludar', new ResourceTemplate('saludar://{nombre}', { list: undefined }), { title: 'Recurso para saludar', description: 'Recurso para saludar a una persona' }, async (url, { nombre }) => {
-    if (typeof nombre !== 'string') {
-        throw new Error('El nombre debe ser una cadena de texto');
-    }
-    return {
-        contents: [
-            {
-                uri: url.href,
-                text: `Hola, saludos terricola ${nombre}`
+                text: `El ${cantidad} ${moneda_origen} es igual a ${value_converted} ${moneda_destino} (Tasa: ${rate.toFixed(6)}, Moneda Base: ${base})`
             }
         ]
     };
